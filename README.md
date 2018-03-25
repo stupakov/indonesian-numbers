040f6cc8472b088a8f16d4160a986f4da2216d91
Alex Stupakov <stupakov@gmail.com>
2018-03-25 12:30:04 +0800

    generate number on component initialization and on button click

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 - validation of typing a known string
 - randomly generate number and get its indonesian name
 - numbers over 9999
+- rename private functions to start with underscore
 - js and jsx plugins for vim
 - fix indentation of html in vim
--- src/components/home.js
+++ src/components/home.js
@@ -2,12 +2,35 @@ const React = require('react');
 const PropTypes = require('prop-types');
 
 class Home extends React.Component {
+	getNewNumber() {
+		return Math.floor(Math.random() * 1000);
+	}
+
+	updateNumber() {
+		this.setState({
+			number: this.getNewNumber()
+		});
+	}
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			number: this.getNewNumber()
+		};
+	}
+
 	render() {
 		return (
 			<div className="jumbotron">
 				<h2>
 					NUMBERS
 				</h2>
+				<button className="reset-button" onClick={() => this.updateNumber()}>
+					Generate new number
+				</button>
+				<div className="number">
+					{this.state.number}
+				</div>
 				<div className="number-string">
 					{this.props.numberAsString}
 				</div>
